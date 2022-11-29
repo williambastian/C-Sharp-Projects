7a391d3d1bdef418126a6fe4f482fbb5b1d73cdc
William <87213388+williambastian@users.noreply.github.com>
2022-11-29 15:29:29 -0600

    Created demo .net core console app

---
+++ Accounting_Console_Demo/Accounting_Console_Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Accounting_Console_Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
