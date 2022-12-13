591ec9712f86f35cd60d988c4fde8a06de3076ad
William <87213388+williambastian@users.noreply.github.com>
2022-12-13 17:07:21 -0600

    created basic demo of custom classes & methods

---
+++ Integer-Methods-Demo/Integer-Methods-Demo/IntMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer_Methods_Demo
+{
+    class IntMath                  // create a class with basic demos of integer arithmetic methods.
+                                   // The arithmetic here is simple for purposes of demonstration.
+    {
+        public int IntSquared(int num1)      //IntSquared() will return the square of a given integer
+        {
+           return (num1 * num1);  
+        }
+
+        public int IntDoubled(int num2)      //IntDoubled() will double the value of a given integer
+        {
+            return (num2 + num2);
+        }
+
+        public int IntTimesTen(int num3)    //IntTimesTen() will multiply a given integer by ten
+        {
+            return (num3 * 10);
+        }
+    }
+}
---
+++ Integer-Methods-Demo/Integer-Methods-Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer_Methods_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntMath intMath = new IntMath();   //to use the methods in the IntMath class, we first instantiate the class
+
+            try //we are taking user input, so structure program in a try-catch block, expecting errors
+            {
+                Console.WriteLine("Enter an integer to be squared:");       //give instructions to the user, request the needed paramater for each class
+                string num1string = Console.ReadLine();                     //user input stored as string
+                int num1 = Convert.ToInt32(num1string);                     //convert to Integer; this is where errors are likely
+                int result1 = intMath.IntSquared(num1);                     //call intMath.IntSquared() method using converted user input as parameter
+                Console.WriteLine(num1 + " squared is " + result1);         // write output of IntSquared() to console
+
+                Console.WriteLine("Enter an integer to be doubled:");       //methodology for IntDoubled() and IntTimesTen() are the same as above
+                string num2string = Console.ReadLine();
+                int num2 = Convert.ToInt32(num2string);
+                int result2 = intMath.IntDoubled(num2);
+                Console.WriteLine(num2 + " doubled is " + result2);
+
+                Console.WriteLine("Enter an integer to be multiplied by ten:");
+                string num3string = Console.ReadLine();
+                int num3 = Convert.ToInt32(num3string);
+                int result3 = intMath.IntTimesTen(num3);
+                Console.WriteLine(num3 + " times 10 is " + result3);
+            }
+            catch (FormatException badFormat)                          //the catch block anticipates invalid format from the user
+            {
+               Console.WriteLine("Please enter a valid integer.");
+            }
+            catch (Exception generalError)                             //catch block for unforseen errors
+            {
+                Console.WriteLine("Unknown error occurred. Please try again.");
+            }
+
+            Console.Read();
+        }
+    }
+}
