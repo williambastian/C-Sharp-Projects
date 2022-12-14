8d7000d243b9841c0e6283eb4c8350959aadb195
William <87213388+williambastian@users.noreply.github.com>
2022-12-14 12:04:35 -0600

    created basic demo of overloading methods

---
+++ Main-Method-Demo/Main-Method-Demo/MathMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Demo
+{
+    class MathMethods
+    {
+        public int someMath(int num1)      //all methods have the same, vague name for demonstration purposes
+        {                                  // this class demonstrates the basics of how methods can be overloaded for unique use cases
+
+            return (num1 + 5);        //if input is integer format, add input to 5
+        }
+
+        public int someMath(decimal num2)
+        {
+            decimal oneDec = 12.00m;                                  // if input is decimal, multiply by 12
+            decimal resultDec = Decimal.Multiply(oneDec, num2);
+            int resultInt = Decimal.ToInt32(resultDec);
+            return resultInt;
+        }
+
+        public int someMath(string num3)
+        {
+            int num3int = Convert.ToInt32(num3);       // if input is string, multiply by 3
+            int resultInt3 = num3int * 3;
+            return resultInt3;
+        }
+    }
+}
---
+++ Main-Method-Demo/Main-Method-Demo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate class containing overloaded method someMath()
+
+            MathMethods mathMethods = new MathMethods();
+
+            try   // each method is dependent on valid input. try-catch anticipates invalid input
+            {
+                // the first version of someMath adds a given integer to 5
+                Console.WriteLine("Please enter an integer to be added to 5");
+                int userInt = Convert.ToInt32(Console.ReadLine());    //user input is converted to int before passing into someMath()
+                int result1 = mathMethods.someMath(userInt);    // someMath receives an integer, and knows to perform +5
+                Console.WriteLine(result1);
+
+                Console.WriteLine("Please enter a decimal to be multiplied by 12");
+                decimal userDec = Convert.ToDecimal(Console.ReadLine());   //user input converted to decimal before passing into someMath()
+                int result2 = mathMethods.someMath(userDec);               //when someMath() receives decimal input, it multiplies input by 12
+                Console.WriteLine(result2);
+
+                Console.WriteLine("Please enter an integer to multiplied by 3");
+                string userString = Console.ReadLine();                       //if user input is not converted, it remains a string
+                int result3 = mathMethods.someMath(userString);               // passing string data into someMath() results in input multiplied by 3
+                Console.WriteLine(result3);
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Format");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Unknown Error");
+            }
+            Console.Read();
+        }
+    }
+}
