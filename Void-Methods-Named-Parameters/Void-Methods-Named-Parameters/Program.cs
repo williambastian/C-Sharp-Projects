190e7a314a6ed912a25be4b77ff2f9e9ea86e0a6
William <87213388+williambastian@users.noreply.github.com>
2022-12-14 15:01:55 -0600

    demo for void methods and named parameters

---
+++ Void-Methods-Named-Parameters/Void-Methods-Named-Parameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Methods_Named_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                VoidMath voidMath = new VoidMath();   //instantiate class containing void method
+                Console.WriteLine("Enter two integers.\nThe first integer will be doubled, and the second integer will be printed without changes.\nEnter the first integer now:");
+                int userNum1 = Convert.ToInt32(Console.ReadLine());     //convert user input to first integer
+                Console.WriteLine("Enter the second integer now:");
+                int userNum2 = Convert.ToInt32(Console.ReadLine());     //convert user input to second integer
+                voidMath.NamedParams(paramTwo: userNum2, paramOne: userNum1);    //call method using specific parameter names. Note these are given out of order, but they function normally
+
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Please enter a valid integer");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Unknown Error");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void-Methods-Named-Parameters/Void-Methods-Named-Parameters/VoidMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Methods_Named_Parameters
+{
+    class VoidMath
+    {
+        //create a void method that takes two integers as parameters.
+        //Do a math operation on the first integer, and display the second integer to the screen
+        public void NamedParams(int paramOne, int paramTwo)              //void method does not strictly return an integer, requires no return statement
+        {                                                                // named parameters can be given out of order, as seen in Program.cs
+            int resultOne = paramOne * 2;          // sample math operation on first parameter
+            Console.WriteLine("Your first integer times two equals: " + resultOne);         //display result of math operation
+            Console.WriteLine("Your second integer: " + paramTwo);                          // display unaltered second parameter
+        }
+    }
+}
