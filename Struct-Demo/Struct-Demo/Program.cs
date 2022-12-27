579926a9c1c059c540f9bbbd07121da0550339e3
William <87213388+williambastian@users.noreply.github.com>
2022-12-27 17:46:26 -0600

    basic struct demo

---
+++ Struct-Demo/Struct-Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Demo
+{
+    class Program
+    {
+        // Step 1: Create struct called Number, give it property "Amount" of type decimal - see Number.cs
+        // Step 2: Create object of data type Number and assign a value to Amount
+        // Print object's amount to console
+
+        static void Main(string[] args)
+        {
+            Number number = new Number();    // create object of Number type
+            number.Amount = 3.50m;           // assign amount
+            Console.WriteLine(number.Amount);                // print amount for specific object to console
+            Console.ReadLine();
+        }
+    }
+}
